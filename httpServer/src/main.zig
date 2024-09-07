9d281872bfdb0870541b2237f2a4d9adcd0cb55b
Tamir Hadash <udbe3333@gmail.com>
2024-09-07 17:30:51 +0300

    better http server and api

--- httpServer/src/main.zig
+++ httpServer/src/main.zig
@@ -5,29 +5,58 @@ const libc = @cImport({
     @cInclude("sys/socket.h");
 });
 
+const httpServer = struct {
+    sockfd: posix.socket_t,
+    address: posix.sockaddr.in,
+
+    fn init(domain: u32, socketType: u32, protocol: u32, backlog: u32, address: posix.sockaddr.in) !httpServer {
+        const sockfd = posix.socket(
+            domain,
+            socketType,
+            protocol,
+        ) catch |err| {
+            std.debug.print("failed to make socket {}", .{err});
+            return err;
+        };
+
+        posix.bind(
+            sockfd,
+            @ptrCast(&address),
+            @sizeOf(@TypeOf(address)),
+        ) catch |err| {
+            std.debug.print("failed to bind {}", .{err});
+            return err;
+        };
+
+        posix.listen(sockfd, @truncate(backlog)) catch |err| {
+            std.debug.print("failed to listen {}", .{err});
+            return err;
+        };
+
+        return .{
+            .sockfd = sockfd,
+            .address = address,
+        };
+    }
+
+    fn deinit(self: httpServer) void {
+        posix.close(self.sockfd);
+    }
+};
+
 pub fn main() !void {
-    const sockfd = posix.socket(
+    var server = try httpServer.init(
         libc.AF_INET,
         libc.SOCK_STREAM,
         0,
-    ) catch |err| {
-        std.debug.print("failed to make socket", .{});
-        return err;
-    };
-    defer posix.close(sockfd);
+        32,
+        .{
+            .family = libc.AF_INET,
+            .port = 8080,
+            .addr = libc.INADDR_ANY,
+        },
+    );
+    defer server.deinit();
 
-    const server_addr: posix.sockaddr.in = .{
-        .family = libc.AF_INET,
-        .port = 8080,
-        .addr = libc.INADDR_ANY,
-    };
-
-    posix.bind(
-        sockfd,
-        &server_addr,
-        @sizeOf(server_addr),
-    ) catch |err| {
-        std.debug.print("failed to bind", .{});
-        return err;
-    };
+    std.debug.print("yippe i have a server\n", .{});
 }
